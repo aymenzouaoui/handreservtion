40b598a8b4dd47ea8f06adcff75d566274c52ecd
aymen <118661290+aymenzouaoui@users.noreply.github.com>
2024-05-29 00:13:10 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please enter a valid email address']
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
